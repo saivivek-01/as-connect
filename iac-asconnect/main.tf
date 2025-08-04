d286db7f4c8689e2ab89a04d08fac7788575155a
MALLAVALLI SAI VIVEK <148973273+saivivek-01@users.noreply.github.com>
2025-08-04 18:26:10 +0530

    Update main.tf

--- iac-asconnect/main.tf
+++ iac-asconnect/main.tf
@@ -1,45 +1,71 @@
-# main.tf
 provider "google" {
-  project = var.project_id
-  region  = var.region
-  zone    = var.zone
+  project = "as-connect-468012"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "asconnect-vpc"
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "asconnect-firewall"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443", "8080"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
 }
 
 resource "google_compute_instance" "asconnect_vm" {
-  name         = var.vm_name
-  machine_type = var.machine_type
-  zone         = var.zone
+  name         = "asconnect-vm"
+  machine_type = "e2-standard-2" # Big instance for 91 days; downsize later manually
+  zone         = "us-central1-a"
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+      image = "debian-cloud/debian-12"
       size  = 20
     }
   }
 
   network_interface {
-    network       = "default"
-    access_config {}
+    network = google_compute_network.vpc_network.name
+
+    access_config {
+      // Required for external IP
+    }
   }
 
-  metadata_startup_script = file("startup-script.sh")
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    apt-get update -y
+    apt-get install -y apt-transport-https ca-certificates curl software-properties-common git
 
-  tags = ["http-server"]
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sh get-docker.sh
+
+    usermod -aG docker ubuntu
+
+    DOCKER_COMPOSE_VERSION="v2.24.4"
+    mkdir -p /usr/local/lib/docker/cli-plugins
+    curl -SL https://github.com/docker/compose/releases/download/1.29.2/docker-compose-linux-x86_64 \
+      -o /usr/local/lib/docker/cli-plugins/docker-compose
+    chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+
+    cd /home
+    git clone https://github.com/saivivek-01/as-connect.git
+    cd as-connect
+    docker compose up -d
+  EOT
+
+  tags = ["http-server", "https-server"]
+
+  service_account {
+    email  = "saivivekmallavalli@gmail.com" # or remove this block to use default
+    scopes = ["cloud-platform"]
+  }
 }
-
-resource "google_compute_firewall" "default" {
-  name    = "allow-http"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["8080"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["http-server"]
-} 
-
-output "instance_ip" {
-  value = google_compute_instance.asconnect_vm.network_interface[0].access_config[0].nat_ip
-} 
